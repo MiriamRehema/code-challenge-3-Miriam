69de4c70a3285c37e2fb7410f35d281a1b997324
MiriamRehema <rehemamiriam30@gmail.com>
2023-07-02 22:16:49 +0300

    index.js

--- index.js
+++ index.js
@@ -33,47 +33,70 @@ fetch(' http://localhost:3000/films')
 
 
   document.addEventListener('DOMContentLoaded', () => {
-    const Container = document.getElementById('container');
+    const Container = document.querySelector('container');
   })
 
-    // Function to populate the movie list in the HTML
-    function populateMovieList(films) {
-      const movieListDiv = document.getElementById('movieList');
-
-      films.forEach(movie => {
+   
         // Create elements for each movie
-        const movieDiv = document.createElement('div');
-        const title = document.createElement('h2');
-        const runtime = document.createElement('p');
-        const capacity = document.createElement('p');
-        const showtime = document.createElement('p');
-        const ticketsSold = document.createElement('p');
-        const description = document.createElement('p');
-        const poster = document.createElement('img');
+        const movieMenu = document.querSelector('menu');
+        const title = document.querSelector('h2');
+        const runtime = document.querSelector('p');
+        const capacity = document.querSelector('p');
+        const showtime = document.querSelector('p');
+        const ticketsSold = document.querSelector('p');
+        const description = document.querSelector('p');
+        const poster = document.querySelector('img');
         // Set the movie information
-        title.textContent = movie.title;
+        title.textContent = film.title;
         runtime.textContent = 'Runtime: ' + movie.runtime + ' minutes';
         capacity.textContent = 'Capacity: ' + movie.capacity;
         showtime.textContent = 'Showtime: ' + movie.showtime;
-        ticketsSold.textContent = 'Tickets Sold: ' + movie.tickets_sold;
+        availableTicketscount.textContent = 'availableTicketscount: ' + movie.availableTicketscount;
         description.textContent = movie.description;
         poster.src = movie.poster;
         poster.alt = movie.title + ' Poster';
 
+       //fuction to update available ticket count when a ticket is purchased
+       const updateavailabelTickets = ()=>{
+        const currentCount=parseInt(availableTicketCount.textContent)
+        if(currentCount > 0){
+          availabeTickets ,count.textCount=current ,Count-1;
+        } 
+      };
+          
+        //function to  ticket handlePurchase
+
+        const handleTicketPurchase= ()=>{
+        const currentCount=parseInt(availableTicketsCount.textContent)
+        if(currentCount  >0){}
+          updateAvailabletTickets();
+          //additional code to handle ticket  purchase.)
+        elseif (
+            alert ,('Sorry tickets are sold out'))
+        }
+         
+        ;
+       //add event listener to the buy ticket button
+       Buy-Ticket.addEventListener("click",handelPurchase)
+       //make a GET request
+
+        
+
+        
         // Append elements to the movieDiv
-        movieDiv.appendChild(title);
-        movieDiv.appendChild(runtime);
-        movieDiv.appendChild(capacity);
-        movieDiv.appendChild(showtime);
-        movieDiv.appendChild(ticketsSold);
-        movieDiv.appendChild(description);
-        movieDiv.appendChild(poster);
+        container.appendChild(title);
+        container.appendChild(runtime);
+        container.appendChild(capacity);
+        container.appendChild(showtime);
+        container.appendChild(ticketsSold);
+        container.appendChild(description);
+        container.appendChild(poster);
 
         
         // Append the movieDiv to the movieListDiv
         movieListDiv.appendChild(movieDiv);
-      });
-    }
+      ;
+    
 
     // Call the  function to start the process
     console.log();{
